0c3cc8bb4987769014866034f6c0eb8d6481f621
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-11-28 22:40:24 +0530

    .

--- client/app.js
+++ client/app.js
@@ -2,6 +2,10 @@ var app = angular.module('bookMyRide', ['ngRoute']);
 app.config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
     $routeProvider
+        .when('/', {
+            templateUrl: '../client/views/home.html',
+            controller: 'HomeController'
+        })
         .when('/home', {
             templateUrl: '../client/views/home.html',
             controller: 'HomeController'
