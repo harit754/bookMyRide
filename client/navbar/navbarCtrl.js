51c75055ec472462ccc8a838cc9ee90804919731
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-11-28 18:38:59 +0530

    .

--- client/navbar/navbarCtrl.js
+++ client/navbar/navbarCtrl.js
@@ -3,16 +3,5 @@ angular.module('bookMyRide')
     .controller('NavbarCtrl', ['$scope', '$location', function
 ($scope, $location) {
 
-        $scope.isActive = function (path) {
-
-            var currentPath = $location.path().split('/')[1];
-
-            if (currentPath.indexOf('?') !== -1) {
-
-                currentPath = currentPath.split('?')[0];
-            }
-            return currentPath === path.split('/')[1];
-
-        };
 
     }]);
