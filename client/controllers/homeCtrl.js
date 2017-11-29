6bcbdf09645f7bab3c7eb1a2b01a1816b6d1f76e
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-11-29 11:42:47 +0530

    .

--- client/controllers/homeCtrl.js
+++ client/controllers/homeCtrl.js
@@ -1,6 +1,7 @@
 angular.module('bookMyRide').controller('homeCtrl', function ($scope, $http, $location) {
 
     $scope.bookARide = function () {
+        alert('Working');
         $location.path('/booking')
     }
 
