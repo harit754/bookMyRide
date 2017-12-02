20ebeaff3e66c7f9e28de1999c5340e7556a5b24
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-02 16:25:23 +0530

    .

--- client/controllers/bookingCtrl.js
+++ client/controllers/bookingCtrl.js
@@ -1,104 +1,115 @@
-angular.module('bookMyRide').controller('bookingCtrl', function ($scope, $http, $location) {
+angular.module('bookMyRide').controller('bookingCtrl', function ($scope, $http, $location, $window) {
 
     $scope.initMap = function () {
-        var pos = { lat: -25.363, lng: 131.044 };
+        var pos = { lat: 28.4595, lng: 77.0266 };
+        $scope.geocoder = new google.maps.Geocoder();
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: pos,
-            styles: [
-                { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-                { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-                { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-                {
-                    featureType: 'administrative.locality',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#d59563' }]
-                },
-                {
-                    featureType: 'poi',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#d59563' }]
-                },
-                {
-                    featureType: 'poi.park',
-                    elementType: 'geometry',
-                    stylers: [{ color: '#263c3f' }]
-                },
-                {
-                    featureType: 'poi.park',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#6b9a76' }]
-                },
-                {
-                    featureType: 'road',
-                    elementType: 'geometry',
-                    stylers: [{ color: '#38414e' }]
-                },
-                {
-                    featureType: 'road',
-                    elementType: 'geometry.stroke',
-                    stylers: [{ color: '#212a37' }]
-                },
-                {
-                    featureType: 'road',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#9ca5b3' }]
-                },
-                {
-                    featureType: 'road.highway',
-                    elementType: 'geometry',
-                    stylers: [{ color: '#746855' }]
-                },
-                {
-                    featureType: 'road.highway',
-                    elementType: 'geometry.stroke',
-                    stylers: [{ color: '#1f2835' }]
-                },
-                {
-                    featureType: 'road.highway',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#f3d19c' }]
-                },
-                {
-                    featureType: 'transit',
-                    elementType: 'geometry',
-                    stylers: [{ color: '#2f3948' }]
-                },
-                {
-                    featureType: 'transit.station',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#d59563' }]
-                },
-                {
-                    featureType: 'water',
-                    elementType: 'geometry',
-                    stylers: [{ color: '#17263c' }]
-                },
-                {
-                    featureType: 'water',
-                    elementType: 'labels.text.fill',
-                    stylers: [{ color: '#515c6d' }]
-                },
-                {
-                    featureType: 'water',
-                    elementType: 'labels.text.stroke',
-                    stylers: [{ color: '#17263c' }]
-                }
-            ]
+            zoom: 16,
+            center: pos
+
         });
+        // var infoWindow = new google.maps.InfoWindow({ map: map });
+
+        // HTML5 geolocation.
+        if ($window.navigator.geolocation) {
+            $window.navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    draggable: true,
+                    map: map,
+                    icon: image,
+                    title: 'Your position'
+                });
+
+                // infoWindow.setPosition(pos);
+                // infoWindow.setContent('Location found.');
+                map.setCenter(pos);
+
+                updateMarkerPosition(marker.getPosition());
+                geocodePosition(pos);
+
+                // Add dragging event listeners.
+                google.maps.event.addListener(marker, 'dragstart', function () {
+                    updateMarkerAddress('Dragging...');
+                });
+
+                google.maps.event.addListener(marker, 'drag', function () {
+                    updateMarkerStatus('Dragging...');
+                    updateMarkerPosition(marker.getPosition());
+                });
+
+                google.maps.event.addListener(marker, 'dragend', function () {
+                    updateMarkerStatus('Drag ended');
+                    geocodePosition(marker.getPosition());
+                    map.panTo(marker.getPosition());
+                });
+
+                google.maps.event.addListener(map, 'click', function (e) {
+                    updateMarkerPosition(e.latLng);
+                    geocodePosition(marker.getPosition());
+                    marker.setPosition(e.latLng);
+                    map.panTo(marker.getPosition());
+                });
+
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+
         var icon = {
             url: "./public/images/car.png", // url
             scaledSize: new google.maps.Size(50, 25), // scaled size
             origin: new google.maps.Point(0, 0), // origin
-            anchor: new google.maps.Point(0, 0) // anchor
+            anchor: new google.maps.Point(0, 32) // anchor
         };
         var image = {
             url: "./public/images/man8.png", // url
             scaledSize: new google.maps.Size(70, 60), // scaled size
             origin: new google.maps.Point(0, 0), // origin
-            anchor: new google.maps.Point(0, 0) // anchor
+            anchor: new google.maps.Point(35, 60) // anchor
         };
 
+    } //initMap() Function ends here.
+
+    function geocodePosition(pos) {
+        $scope.geocoder.geocode({
+            latLng: pos
+        }, function (responses) {
+            if (responses && responses.length > 0) {
+                updateMarkerAddress(responses[0].formatted_address);
+            } else {
+                updateMarkerAddress('Cannot determine address at this location.');
+            }
+        });
+    }
+
+    function updateMarkerStatus(str) {
+        document.getElementById('markerStatus').innerHTML = str;
+    }
+
+    function updateMarkerPosition(latLng) {
+        document.getElementById('info').innerHTML = [
+            latLng.lat(),
+            latLng.lng()
+        ].join(', ');
+    }
+
+    function updateMarkerAddress(str) {
+        document.getElementById('address').innerHTML = str;
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
         var marker = new google.maps.Marker({
             position: pos,
             map: map,
