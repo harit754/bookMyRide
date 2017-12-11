275112c1ed95f20fa1057180c4791a03b5091e4a
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-11 12:47:03 +0530

    .

--- client/controllers/loginCtrl.js
+++ client/controllers/loginCtrl.js
@@ -1,12 +1,16 @@
 angular.module('bookMyRide').controller('loginCtrl', function ($scope, $http, $location, $window) {
-    $scope.showLogin = true;
-    $scope.newUser = {};
-    $scope.loginUser = {};
+    init();
+    function init() {
+        $scope.showLogin = true;
+        $scope.newUser = {};
+        $scope.loginUser = {};
+    }
+
     $scope.signUp = function () {
         this.newUser.role = 'User';
         $http.post('/user/sign-up', $scope.newUser).then(function (response) {
             console.log('Data Saved Successfully');
-            alert('Data Saved Successfully');
+            init();
         });
     }
 });
