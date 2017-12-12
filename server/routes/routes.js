90dca5c91e8794ca403f160d64fd7086b4a648af
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-12 15:02:57 +0530

    .

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,7 +6,6 @@ var bcrypt = require('bcrypt');
 router.post('/sign-up', function (req, res) {
     console.log(req.body);
 
-
     bcrypt.hash(req.body.password, 5, function (err, hashPassword) {
         if (err) {
             throw err;
@@ -31,37 +30,40 @@ router.post('/sign-up', function (req, res) {
             });
         }
     });
-
-
-
-
 });
 
 router.post('/add-driver', function (req, res) {
     console.log(req.body);
-    var NewUser = new user({
-        role: 'Driver',
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phoneNumber: req.body.phoneNumber,
-        password: req.body.password,
-        licenseNo: req.body.licenseNo,
-        licenseExp: req.body.licenseExp,
-        experience: req.body.experience,
-        cabNo: req.body.cabNo,
-        cabType: req.body.cabType,
-        model: req.body.model,
-        color: req.body.color
-    });
-    NewUser.save(function (err, data) {
+    bcrypt.hash(req.body.password, 5, function (err, hashPassword) {
         if (err) {
             throw err;
         } else {
-            console.log('New Driver Added Successfully');
-            res.end();
+            var NewUser = new user({
+                role: 'Driver',
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                phoneNumber: req.body.phoneNumber,
+                password: hashPassword,
+                licenseNo: req.body.licenseNo,
+                licenseExp: req.body.licenseExp,
+                experience: req.body.experience,
+                cabNo: req.body.cabNo,
+                cabType: req.body.cabType,
+                model: req.body.model,
+                color: req.body.color
+            });
+            NewUser.save(function (err, data) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log('New Driver Added Successfully');
+                    res.end();
+                }
+            });
         }
     });
+
 });
 
 module.exports = router;    
