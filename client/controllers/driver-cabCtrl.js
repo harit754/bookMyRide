9cf57e41295da93ba4a9373f5de2bd03c30ac810
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-13 03:14:27 +0530

    .

--- client/controllers/driver-cabCtrl.js
+++ client/controllers/driver-cabCtrl.js
@@ -1,5 +1,17 @@
 angular.module('bookMyRide').controller('driver-cabCtrl', function ($scope, $http, $location, $window) {
+    init();
+    function init() {
+        $scope.showCab = true;
+        $scope.newUser = {};
+    }
 
+    $scope.signUp = function () {
+        this.newUser.role = 'Driver';
+        $http.post('/user/sign-up', $scope.newUser).then(function (response) {
+            console.log('Data Saved Successfully');
+            init();
+        });
+    }
 
 
 });
