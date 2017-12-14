7efe1c76505e3ab1c8476dfeead745b6964fd185
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-14 11:16:29 +0530

    .

--- client/controllers/tariffCtrl.js
+++ client/controllers/tariffCtrl.js
@@ -1,5 +1,17 @@
 angular.module('bookMyRide').controller('tariffCtrl', function ($scope, $http, $location, $window) {
+    init();
+    function init() {
+        $scope.newTariff = {};
+    }
 
+
+    $scope.addTariff = function () {
+
+        $http.post('/user/add-tariff', $scope.newTariff).then(function (response) {
+            console.log('Data Saved Successfully');
+            init();
+        });
+    }
 
 
 });
