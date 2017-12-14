662bbc4a2c94e0173f110929ef1e0da979dc1ff4
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-14 19:11:59 +0530

    .

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '/node_modules')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', express.static(path.join(__dirname, '/client')));
 
 app.use('/user', userRoute);
