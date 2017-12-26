393d0ae04a60873714d7323ca31d5c2c33493d5a
ayushpratap2494 <32447637+ayushpratap2494@users.noreply.github.com>
2017-12-26 19:43:08 +0530

    .

--- client/app.js
+++ client/app.js
@@ -43,7 +43,7 @@ app.config(function ($routeProvider, $locationProvider, $httpProvider) {
         })
         .when('/booking-history', {
             templateUrl: './views/booking-history.html',
-            controller: 'bookingCtrl'
+            controller: 'loginCtrl'
         });
 
     $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function ($q, $location, $localStorage) {
